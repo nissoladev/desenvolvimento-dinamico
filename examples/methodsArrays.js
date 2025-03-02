b48797f0c0776d558d6216ca8b371be2c8ee6fff
Guilherme Nissola <nissoladev@gmail.com>
2025-03-02 18:02:59 -0300

    Complementando arquivo methodsArrays.js com novos métodos

--- examples/methodsArrays.js
+++ examples/methodsArrays.js
@@ -1,14 +1,28 @@
+let numbers = [90, 50, 77, 60];
 let arrayString = ['Gaúcha', 'Paulista', 'Francesa'];
 console.log(arrayString);
-// Separador entre índices do array
-//console.log(arrayString.join(' '));
+// Join - Separador entre índices do array
+console.log(arrayString.join(' / '));
 
+// Sort - Ordena os valores do array.
+console.log(numbers.sort().join(' - '));
 
-// Ordena os valores do array
-//console.log(arrayString.sort());
+// Reverse - Inverte a ordem dos elementos. O primeiro torna-se o último e o último torna-se o primeiro.
+console.log(numbers.reverse().join(' - '));
 
+// Slice - Retorna os elementos selecionados de um array.
+console.log(numbers.slice(2,4).join(' / '));
 
+// Length - Mostra o número de elementos em um array.
+console.log(numbers.length);
 
-// Retorna o primeiro valor de array e remove os demais.
-//console.log(arrayString.shift());
+// Push - Adiciona valores ao final de um array, retornando o seu novo tamanho.
+console.log(numbers.push(33, 47));
 
+// Shift - Retorna o primeiro valor de array e remove os demais.
+console.log(numbers.shift());
+
+// Pop - Retorna o último elemento de um array e remove os demais.
+console.log(numbers.pop());
+
+console.log(typeof(numbers));
